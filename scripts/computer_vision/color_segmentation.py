e5ff34e1bb4158b66e5cf00c210b913de9e89a46
fermata-nick <85192127+fermata-nick@users.noreply.github.com>
2023-03-15 16:12:12 -0400

    added color segmentation code to git

--- scripts/computer_vision/color_segmentation.py
+++ scripts/computer_vision/color_segmentation.py
@@ -34,10 +34,40 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
-
-	bounding_box = ((0,0),(0,0))
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+	# orange ranges in hues from 30-45
+	#image_print(hsv_img)
+	# settings for good performance on testing: (0, 220, 90), (45, 255, 255)
+	mask = cv2.inRange(hsv_img, (0, 220, 150), (45, 255, 255))
+	# black = cv2.inRange(hsv_img, (10, 10, 10), (11, 11, 11))
+	# Use erosion and dilation to isolate 
+	image_print(mask)
+	kernel = np.ones((5,5), np.uint8)
+	d_kern = np.ones((5,5), np.uint8)
+	eroded = cv2.erode(mask, kernel)
+	dilated = cv2.dilate(eroded, d_kern)
+	# image_print(eroded)
+	image_print(dilated)
+	# Extract contours
+	contours, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+	outlined = cv2.drawContours(img, contours, -1, (0, 255, 255), 3)
+	# print(len(contours))
+	# image_print(outlined)
+	sortedContours = sorted(contours, key=cv2.contourArea, reverse=True)
+	if (len(contours) > 0):
+		x,y,w,h = cv2.boundingRect(sortedContours[0])
+	else:
+		x,y,w,h = (0,0,0,0)
+		print("missed bb")
+		image_print(img)
+	bounding_box = ((x,y),(x+w,y+h))
+	bounding_img = cv2.rectangle(img, bounding_box[0], bounding_box[1], (0,255,0),2)
+	image_print(bounding_img)
 
 	########### YOUR CODE ENDS HERE ###########
-
 	# Return bounding box
 	return bounding_box
+
+if __name__ == "__main__":
+	img = cv2.imread("./test_images_cone/test9.jpg")
+	cd_color_segmentation(img, "template holder")
