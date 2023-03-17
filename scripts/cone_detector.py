ddada2a91a5d35120e77f43680b86f2d0b3fd81e
fermata-nick <85192127+fermata-nick@users.noreply.github.com>
2023-03-16 21:04:02 -0400

    added values for empty bboxes

--- scripts/cone_detector.py
+++ scripts/cone_detector.py
@@ -54,8 +54,12 @@ class ConeDetector():
         bbox = cd_color_segmentation(image, "no_template")
         pixel_msg = ConeLocationPixel()
         # publishing middle x and bottom y of bbox
-        pixel_msg.u = (bbox[0][0] + bbox[1][0]) * 0.5
-        pixel_msg.v = bbox[1][1]
+        if (bbox[1] == (0,0)):
+            pixel_msg.u = -1
+            pixel_msg.v = -1
+        else:
+            pixel_msg.u = (bbox[0][0] + bbox[1][0]) * 0.5
+            pixel_msg.v = bbox[1][1]
         self.cone_pub.publish(pixel_msg)
         debug_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
         self.debug_pub.publish(debug_msg)
